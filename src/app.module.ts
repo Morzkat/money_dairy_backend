4f6ea84609268e9580e4b3e622151aa90098cb5f
Morzkat <felix.david21@outlook.com>
2024-02-29 18:06:49 -0400

    * Add new wallet module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionsModule } from './transactions/transactions.module';
+import { WalletsModule } from './wallets/wallets.module';
 
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, WalletsModule],
   controllers: [],
   providers: [],
 })
