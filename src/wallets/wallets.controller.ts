a84ecb913bcd3670ea44b27fb449e9b6eb02bdeb
Morzkat <felix.david21@outlook.com>
2024-03-07 10:50:09 -0400

    * setup ConfigModule. * Add logic to wallet controller. * Add new WalletRepository dependency. * Add logic to walletService.

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Wallet } from './wallet.type';
+import { WalletsService } from './wallets.service';
+import { Response } from './../core/entities/response.type';
 
 @Controller('wallets')
-export class WalletsController { }
+export class WalletsController {
+    constructor(private walletService: WalletsService) { }
+
+    @Get()
+    async getWallets() {
+        return new Response<Array<Wallet>>(
+            'Wallets',
+            await this.walletService.getAll(),
+        );
+    }
+
+    @Get(':id')
+    async getWalletById(@Param('id') id: number) {
+        return new Response<Wallet>(
+            'Wallet details',
+            await this.walletService.getById(id),
+        );
+    }
+
+    @Post()
+    async createWallet(@Body() wallet: Wallet) {
+        return new Response<Wallet>(
+            'New wallet created',
+            await this.walletService.create(wallet),
+        );
+    }
+
+    @Put(':id')
+    async updateWallet(@Body() wallet: Wallet, @Param('id') id: number) {
+        return new Response<void>(
+            'Wallet updated',
+            await this.walletService.update(id, wallet),
+        );
+    }
+
+    @Delete(':id')
+    async deleteWallet(@Param('id') id: number) {
+        return new Response<void>(
+            'Wallet deleted',
+            await this.walletService.delete(id),
+        );
+    }
+}
--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WalletsController } from './wallets.controller';
 import { WalletsService } from './wallets.service';
+import { WalletsRepository } from 'src/core/db/unitOfWork/repositories/wallets.repository';
 
 @Module({
   controllers: [WalletsController],
-  providers: [WalletsService],
+  providers: [WalletsRepository, WalletsService],
 })
 export class WalletsModule { }
