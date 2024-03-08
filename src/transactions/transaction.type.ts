7867b03fc67051c4ac9bfa4136a8217f3aabd57e
Morzkat <felix.david21@outlook.com>
2024-03-08 09:59:17 -0400

    * Add new dto types for: - Status - Categories. * Update transaction controller. * Add repository injections. * Update logic service to use repositories.

--- src/transactions/transaction.type.ts
+++ src/transactions/transaction.type.ts
@@ -1,16 +1,36 @@
+import { BaseEntity } from "src/shared/base.entity.type";
+import { Wallet } from "src/wallets/wallet.type";
+
 export enum Status {
     Paid = 'Paid',
+}
+
+export type StatusToInsertOrUpdate = BaseEntity & {
+    name: string,
 }
 
 export enum Category {
     Grocery = 'Grocery',
 }
 
+export type CategoryToInsertOrUpdate = BaseEntity & {
+    name: string,
+}
+
 //TODO: Create a smaller type with only amount and
-export type Transaction = {
-    id: string;
-    amount: number;
-    date: string;
-    status: Status;
-    category: Category;
+export type Transaction = BaseEntity & {
+    amount: string;
+    wallet: Wallet,
+    createAt: Date;
+    modifyAt: Date;
+    status: Status | string;
+    category: Category | string;
 };
+
+// Add validator...
+export type TransactionToInsertOrUpdate = {
+    category: string,
+    status: string
+    wallet: number,
+    amount: string,
+}
